bb771bf7ccd8037fac9c899ef5a462a05486b62e
andrelife <andrelife2104@gmail.com>
2019-05-24 12:00:28 +0200

    modal 1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 window.addEventListener('DOMContentLoaded', function(){
     //modal
-    let popupEngineer = document.getElementsByClassName('popup_engineer'),
-        callBtn = document.getElementsByClassName('popup_engineer_btn'),
-        close = document.querySelector('popup_close');
-        console.log(popupEngineer);
-        callBtn.addEventListener('click', function(){
+    let popupEngineer = document.querySelectorAll('.popup_engineer')[0],
+        callBtn = document.querySelectorAll('.popup_engineer_btn')[0],
+        close = document.querySelector('.popup_close');
+        console.log(close);
+        callBtn.addEventListener('click', function() {
             popupEngineer.style.display = 'block';
         });
+        // close.addEventListener('click', function() {
+        //     popupEngineer.style.display = 'none';
+        // });
 
 });
