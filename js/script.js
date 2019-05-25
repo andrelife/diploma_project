1183c07f8add5d1f9646efaa5032a89a8b67b420
andrelife <andrelife2104@gmail.com>
2019-05-25 20:49:19 +0200

    форма не знаю куда вставить проверку - все рушит проверка

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 window.addEventListener('DOMContentLoaded', function(){
     //modal
     let popupEngineer = document.querySelector('.popup_engineer'),
+        popup = document.querySelector('#pup'),
         callBtn = document.querySelector('.popup_engineer_btn'),
         close = document.querySelectorAll('.popup_close'),
         contactUs = document.querySelector('.contact_us_wrap');
+        //console.log(close);
 
     function openModal() {
-            popupEngineer.style.display = 'block';
+            popup.style.display = 'block';
             document.body.style.overflow = 'hidden';
         }
         setTimeout(openModal, 61000);
@@ -15,63 +17,99 @@ window.addEventListener('DOMContentLoaded', function(){
         popupEngineer.style.display = 'block';
         document.body.style.overflow = 'hidden';
     });
+    popup.addEventListener('click', function(event){
+        let target = event.target;
+        if(target || target.classList.contains('popup_close')){
+            for(let i = 0; i < close.length; i++) {
+                if(target == close[i]){
+                popupEngineer.style.display = 'none';
+                        document.body.style.overflow = '';
+                }
+            }
+        }
+    });
+    
     for(let i = 0; i < close.length; i++) {
         close[i].addEventListener('click', function() {
-            
+                popup.style.display = 'none';
                 popupEngineer.style.display = 'none';
                 document.body.style.overflow = '';
         
         });
     }
-    popupEngineer.addEventListener('click', function(){
-        popupEngineer.style.display = 'none';
-        document.body.style.overflow = '';
-    });
     contactUs.addEventListener('click', function() {
-        popupEngineer.style.display = 'block';
+        popup.style.display = 'block';
         document.body.style.overflow = 'hidden';
     });
+
     //form
     let message = {
         loading: 'Идет отправка...',
         success: 'Отправлено! Скоро мы с вами свяжемся!',
         failure: 'Ошибка...' 
     };
-    let form = document.querySelectorAll('.form')[7],
-        input = document.getElementsByClassName('input'),
+    let form = document.getElementsByClassName('form'),
+        input = document.getElementsByTagName('input'),
+        formMain = document.querySelectorAll('.main_form'),
         statusMessage = document.createElement('div');
-        console.log(form);
+        //console.log(inputNamePhone);
     
     statusMessage.classList.add('status');
-    //for(let i = 0; i < form.length; i++) {
-        form.addEventListener('submit', function(event) {
+    const sendForm = (elem) => {
+        elem.addEventListener('submit', (event) => {
             event.preventDefault();
-            form.appendChild(statusMessage);
+            elem.appendChild(statusMessage);
+            let formData = new FormData(elem);
+                
+            function posData(data) {
+                return new Promise((resolve, reject) => {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-Type',
+                        'application/json; charset=utf-8');
 
-    let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    let formData = new FormData(form);
-    
-    let obj = {};
-        formData.forEach(function(value, key) {
-            obj[key] = value;
+                    request.onreadystatechange = function () {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4) {
+                            if (request.status === 200 && request.status < 3) {
+                                resolve();
+                            } else {
+                                reject();
+                            }
+                        }
+                    };
+                    let obj = {};
+                    formData.forEach((value, key) => {
+                        obj[key] = value;
+                    });
+                    let json = JSON.stringify(obj);
+                    request.send(json);
+                });
+            }
+
+            const clearInput = () => {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            };
+            
+            posData(formData)
+                .then(() => statusMessage.innerHTML = message.loading)
+                .then(() => statusMessage.innerHTML = message.success)
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(clearInput);
         });
-    let json = JSON.stringify(obj);
-
-    request.send(json);
-    request.addEventListener('readystatechange', function(){
-        if(request.readyState < 4) {
-            statusMessage.innerHTML = message.loading; 
-        }else if(request.readyState === 4 && request.status == 200) {
-            statusMessage.innerHTML = message.success;
-        }else{
-            statusMessage.innerHTML = message.failure;
-        }
-    });
+    };
+    for(let i = 0; i < form.length; i++){
+        sendForm(form[i]); 
+        break;
+    }
+    sendForm(form);
+    
+    console.log(form);
 
 
-    });
 
 
 });
