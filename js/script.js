d602fe35f642028e84d12a7848f32ee0bc4750db
andrelife <andrelife2104@gmail.com>
2019-05-28 22:01:28 +0200

    правки форм

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', function(){
         popupCloseTag = document.querySelector('.popup strong'),
         popupCloseTag1 = document.querySelector('.popup_engineer strong'),
         contactUs = document.querySelector('.contact_us_wrap a');
-        console.log(popupCloseTag1);
+        //console.log('hello');
 
     function openModal(elem) {
             elem.style.display = 'block';
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', function(){
             event.preventDefault();
             openModal(popup);
         }
-        if(target == popupClose[0] || target == popupClose[1] || target == popupCloseTag || target == popupCloseTag1 || target == popupEngineer || target == popup){
+        if( target == popupCloseTag || target == popupCloseTag1 || target == popupEngineer || target == popup){
             closeModal(popupEngineer);
             closeModal(popup);  
         }
@@ -126,161 +126,27 @@ window.addEventListener('DOMContentLoaded', function(){
     for(let i = 0; i < form.length; i++){
         sendForm(form[i]); 
     }
-    //sendForm(form);
+    let phoneMask = document.querySelectorAll('.mask');
+    phoneMask.forEach((item) => {
+        item.addEventListener('keyup', function(){
+            //console.log(this.value);
+            this.value = '+' + this.value.replace(/[^\d]/g, '').slice(0, 11);
+        });
+    })
+    
 //tab
-// let glazing_slider = document.querySelector('.glazing_slider'),
-// tree_link = glazing_slider.querySelector('.tree_link'),
-// aluminum_link = glazing_slider.querySelector('.aluminum_link'),
-// plastic_link = glazing_slider.querySelector('.plastic_link'),
-// french_link = glazing_slider.querySelector('.french_link'),
-// rise_link = glazing_slider.querySelector('.rise_link'),
 
-// tree = document.querySelector('.tree'),
-// aluminum = document.querySelector('.aluminum'),
-// plastic = document.querySelector('.plastic'),
-// french = document.querySelector('.french'),
-// rise = document.querySelector('.rise');
+let tab = document.querySelectorAll('.glazing_block a'),//ссылки на табы
+    info = document.querySelector('.glazing_slider'),//родитель 
+    tabContent = document.querySelectorAll('#tab_content'),// контент
+    tabwrap = document.querySelectorAll('.glazing_block img');// картинки над табами
+    console.log(tabwrap);
 
-// //Activating tabs 
-// const tabActive = (a) => {
-// a.classList.add('active');
-// };
-
-// //Deactivating tabs
-// const tabDeactive = (d) => {
-// d.classList.remove('active');
-// };
-
-// //Displaing content
-// const contDisplay = (e) => {
-// e.style.display = 'block';
-// };
-
-// //Hiding content
-// const contDisplayNone = (n) => {
-// n.style.display = 'none';
-// };
-
-// glazing_slider.addEventListener('click', function (e) {
-// let target = e.target;
-
-// //tree active
-// if (target.classList.contains('tree_link')) {
-//     //link active/deactive
-//     tabActive(tree_link);
-
-//     tabDeactive(aluminum_link);
-//     tabDeactive(plastic_link);
-//     tabDeactive(french_link);
-//     tabDeactive(rise_link);
-//     //end
-
-//     //content display/none
-//     contDisplay(tree);
-
-//     contDisplayNone(aluminum);
-//     contDisplayNone(plastic);
-//     contDisplayNone(french);
-//     contDisplayNone(rise);
-//     //end
-// }
-
-// //aluminum active
-// if (target.classList.contains('aluminum_link')) {
-//     //link active/deactive
-//     tabActive(aluminum_link);
-
-//     tabDeactive(tree_link);
-//     tabDeactive(plastic_link);
-//     tabDeactive(french_link);
-//     tabDeactive(rise_link);
-//     //end
-
-//     //content display/none
-//     contDisplay(aluminum);
-
-//     contDisplayNone(tree);
-//     contDisplayNone(plastic);
-//     contDisplayNone(french);
-//     contDisplayNone(rise);
-//     //end
-// }
-
-// //plastic active
-// if (target.classList.contains('plastic_link')) {
-//     //link active/deactive
-//     tabActive(plastic_link);
-
-//     tabDeactive(tree_link);
-//     tabDeactive(aluminum_link);
-//     tabDeactive(french_link);
-//     tabDeactive(rise_link);
-//     //end
-
-//     //content display/none
-//     contDisplay(plastic);
-
-//     contDisplayNone(tree);
-//     contDisplayNone(aluminum);
-//     contDisplayNone(french);
-//     contDisplayNone(rise);
-//     //end
-// }
-
-// //french active
-// if (target.classList.contains('french_link')) {
-//     //link active/deactive
-//     tabActive(french_link);
-
-//     tabDeactive(tree_link);
-//     tabDeactive(aluminum_link);
-//     tabDeactive(plastic_link);
-//     tabDeactive(rise_link);
-//     //end
-
-//     //content display/none
-//     contDisplay(french);
-
-//     contDisplayNone(tree);
-//     contDisplayNone(aluminum);
-//     contDisplayNone(plastic);
-//     contDisplayNone(rise);
-//     //end
-// }
-
-// //rise active
-// if (target.classList.contains('rise_link')) {
-//     //link active/deactive
-//     tabActive(rise_link);
-
-//     tabDeactive(tree_link);
-//     tabDeactive(aluminum_link);
-//     tabDeactive(plastic_link);
-//     tabDeactive(french_link);
-//     //end
-
-//     //content display/none
-//     contDisplay(rise);
-
-//     contDisplayNone(tree);
-//     contDisplayNone(aluminum);
-//     contDisplayNone(plastic);
-//     contDisplayNone(french);
-//     //end
-// }
-// });
-
-
-let tab = document.querySelectorAll('.glazing_block a'),
-tabwrap = document.querySelectorAll('.glazing_block'),
-info = document.querySelector('.glazing_slider'),
-tabContent = document.querySelectorAll('#tab_content');
-
-function hideTabContent(a) {
-for (let i = a; i < tabContent.length; i++) {
-tabContent[i].classList.remove('show');
-tabContent[i].classList.add('hide');
-}
+    function hideTabContent(a) {
+    for (let i = a; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add('hide');
+    }
 }
 
 hideTabContent(1);
@@ -332,7 +198,7 @@ function getTimeRemaining(endtime){
         days = Math.floor((t/(1000*60*60*24)));
         return {
             'total' : t,
-            'days' : ('0' + days).slice(-2),
+            'days' : days,
             'hours' : ('0' + hours).slice(-2),
             'minutes' :('0' + minutes).slice(-2),
             'seconds' : ('0' + seconds).slice(-2)
